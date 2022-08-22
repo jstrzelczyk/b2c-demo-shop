0ef9d58b4e993580c2bae728b472a714b37f9efd
Jagoda Strzelczyk <jagoda.strzelczyk@accenture.com>
2022-08-22 20:40:07 +0200

    Bootcamp: Different Stores, different logic

---
+++ src/Pyz/Zed/Application/Communication/Controller/IndexController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Pyz\Zed\Application\Communication\Controller;
+
+use Spryker\Zed\Application\Communication\Controller\IndexController as SprykerIndexController;
+
+use Symfony\Component\HttpFoundation\Response;
+
+class IndexController extends SprykerIndexController
+{
+    /**
+     * @param \Symfony\Component\HttpFoundation\Response $response
+     *
+     *
+     */
+    public function indexAction()
+    {
+        return new Response('Hello DE Store!');
+    }
+}
