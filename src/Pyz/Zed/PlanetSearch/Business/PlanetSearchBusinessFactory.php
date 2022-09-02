920f13040dcbb5df6f84fa8a9362e116c6854dfc
Jagoda Strzelczyk <jagoda.strzelczyk@accenture.com>
2022-09-02 13:20:44 +0200

    Publish & Synchronize - ElasticSearch

---
+++ src/Pyz/Zed/PlanetSearch/Business/PlanetSearchBusinessFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Pyz\Zed\PlanetSearch\Business;
+
+use Pyz\Zed\PlanetSearch\Business\Writer\PlanetSearchWriter;
+
+class PlanetSearchBusinessFactory
+{
+
+    /**
+
+     * @return \Pyz\Zed\PlanetSearch\Business\Writer\PlanetSearchWriter
+
+     */
+
+    public function createPlanetSearchWriter()
+
+    {
+
+        return new PlanetSearchWriter();
+
+    }
+
+}
---
+++ src/Pyz/Zed/PlanetSearch/Business/PlanetSearchFacadeInterface.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Pyz\Zed\PlanetSearch\Business;
+
+interface PlanetSearchFacadeInterface
+{
+
+    /**
+
+     * @param int $idPlanet
+
+     *
+
+     * @return void
+
+     */
+
+    public function publish(int $idPlanet): void;
+
+}
