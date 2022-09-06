98a5db87b85c5d82b6c3ad1934a470dbdc8a915e
Jagoda Strzelczyk <jagoda.strzelczyk@accenture.com>
2022-09-06 09:33:22 +0200

    Search Client - ElasticSearch

---
+++ src/Pyz/Client/Planet/PlanetClientInterface.php
@@ -0,0 +1,25 @@
+<?php
+
+
+
+namespace Pyz\Client\Planet;
+
+
+
+interface PlanetClientInterface
+{
+
+    /**
+
+     * @param string $name
+
+     *
+
+     * @return array
+
+     */
+
+    public function getPlanetByName(string $name): array;
+
+}
+
