f296d9395a620336224f4ce375032ebcb7e3a8dd
Jagoda Strzelczyk <jagoda.strzelczyk@accenture.com>
2022-09-08 14:52:51 +0200

    frontend

---
+++ src/Pyz/Yves/PlanetWidget/PlanetWidgetFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Pyz\Yves\PlanetWidget;
+
+use Pyz\Client\Planet\PlanetClientInterface;
+
+use Spryker\Yves\Kernel\AbstractFactory;
+
+class PlanetWidgetFactory extends AbstractFactory
+
+{
+
+    /**
+
+     * @return \Pyz\Client\Planet\PlanetClientInterface
+
+     */
+
+    public function getPlanetClient(): PlanetClientInterface
+
+    {
+
+        return $this->getProvidedDependency(PlanetWidgetDependencyProvider::CLIENT_PLANET);
+
+    }
+
+}
